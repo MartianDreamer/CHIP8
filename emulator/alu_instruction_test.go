f001fbf20b549fefece41eadea0cf8480ebebc39
MartianDreamer <nguyenxuansang9494@gmail.com>
2024-05-05 18:13:34 +0000

    add tests for alu ins

--- emulator/alu_instruction_test.go
+++ emulator/alu_instruction_test.go
@@ -2,7 +2,6 @@ package emulator
 
 import (
 	"testing"
-
 )
 
 func Test_exec_set_vx_kk(t *testing.T) {
@@ -42,18 +41,17 @@ func Test_exec_set_vx_rand_and_kk(t *testing.T) {
 func Test_set_vx_vy(t *testing.T) {
 	emulator := Make_chip8(500)
 	emulator.v[0x2] = 0xfa
-	emulator.exec_opcode_8_ins([2]byte{0x80,0x20})
+	emulator.exec_opcode_8_ins([2]byte{0x80, 0x20})
 	if emulator.v[0x0] != emulator.v[0x2] {
 		t.Fatalf("v[0]: %x", emulator.v[0x0])
 	}
 }
 
-
 func Test_set_vx_vx_or_vy(t *testing.T) {
 	emulator := Make_chip8(500)
 	emulator.v[0x0] = 0b11110000
 	emulator.v[0x2] = 0b11000011
-	emulator.exec_opcode_8_ins([2]byte{0x80,0x21})
+	emulator.exec_opcode_8_ins([2]byte{0x80, 0x21})
 	if emulator.v[0x0] != 0b11110011 {
 		t.Fatalf("v[0]: %x", emulator.v[0x0])
 	}
@@ -63,7 +61,7 @@ func Test_set_vx_vx_and_vy(t *testing.T) {
 	emulator := Make_chip8(500)
 	emulator.v[0x0] = 0b11110000
 	emulator.v[0x2] = 0b11000011
-	emulator.exec_opcode_8_ins([2]byte{0x80,0x22})
+	emulator.exec_opcode_8_ins([2]byte{0x80, 0x22})
 	if emulator.v[0x0] != 0b11000000 {
 		t.Fatalf("v[0]: %x", emulator.v[0x0])
 	}
@@ -72,7 +70,7 @@ func Test_set_vx_vx_xor_vy(t *testing.T) {
 	emulator := Make_chip8(500)
 	emulator.v[0x0] = 0b11110000
 	emulator.v[0x2] = 0b11000011
-	emulator.exec_opcode_8_ins([2]byte{0x80,0x23})
+	emulator.exec_opcode_8_ins([2]byte{0x80, 0x23})
 	if emulator.v[0x0] != 0b00110011 {
 		t.Fatalf("v[0]: %x", emulator.v[0x0])
 	}
@@ -82,29 +80,27 @@ func Test_set_vx_vx_add_vy_less_than_0xff(t *testing.T) {
 	emulator := Make_chip8(500)
 	emulator.v[0x0] = 0b00110000
 	emulator.v[0x2] = 0b11000011
-	emulator.exec_opcode_8_ins([2]byte{0x80,0x24})
+	emulator.exec_opcode_8_ins([2]byte{0x80, 0x24})
 	if emulator.v[0x0] != 0b11110011 || emulator.v[0xf] != 0x0 {
 		t.Fatalf("v[0]: %x", emulator.v[0x0])
 	}
 }
 
-
 func Test_set_vx_vx_add_vy_greater_than_0xff(t *testing.T) {
 	emulator := Make_chip8(500)
 	emulator.v[0x0] = 0b01110000
 	emulator.v[0x2] = 0b11000011
-	emulator.exec_opcode_8_ins([2]byte{0x80,0x24})
+	emulator.exec_opcode_8_ins([2]byte{0x80, 0x24})
 	if emulator.v[0x0] != 0b00110011 || emulator.v[0xf] != 0x1 {
 		t.Fatalf("v[0]: %x", emulator.v[0x0])
 	}
 }
 
-
 func Test_set_vx_vx_substract_vy_vx_greater_than_vy(t *testing.T) {
 	emulator := Make_chip8(500)
 	emulator.v[0x0] = 195
 	emulator.v[0x2] = 112
-	emulator.exec_opcode_8_ins([2]byte{0x80,0x25})
+	emulator.exec_opcode_8_ins([2]byte{0x80, 0x25})
 	if emulator.v[0x0] != 83 || emulator.v[0xf] != 0x1 {
 		t.Fatalf("v[0]: %d", emulator.v[0x0])
 	}
@@ -114,7 +110,7 @@ func Test_set_vx_vx_substract_vy_vx_less_than_vy(t *testing.T) {
 	emulator := Make_chip8(500)
 	emulator.v[0x0] = 0b01111111
 	emulator.v[0x2] = 0b11111111
-	emulator.exec_opcode_8_ins([2]byte{0x80,0x25})
+	emulator.exec_opcode_8_ins([2]byte{0x80, 0x25})
 	if emulator.v[0x0] != 0b10000000 || emulator.v[0xf] != 0x0 {
 		t.Fatalf("v[0]: %d", emulator.v[0x0])
 	}
@@ -123,12 +119,12 @@ func Test_set_vx_vx_substract_vy_vx_less_than_vy(t *testing.T) {
 func Test_set_vx_shift_right_vx(t *testing.T) {
 	emulator := Make_chip8(500)
 	emulator.v[0x0] = 0b00000011
-	emulator.exec_opcode_8_ins([2]byte{0x80,0x26})
+	emulator.exec_opcode_8_ins([2]byte{0x80, 0x26})
 	if emulator.v[0x0] != 0b00000001 || emulator.v[0xf] != 0x1 {
 		t.Fatalf("v[0]: %d", emulator.v[0x0])
 	}
 	emulator.v[0x0] = 0b00000010
-	emulator.exec_opcode_8_ins([2]byte{0x80,0x26})
+	emulator.exec_opcode_8_ins([2]byte{0x80, 0x26})
 	if emulator.v[0x0] != 0b00000001 || emulator.v[0xf] != 0x0 {
 		t.Fatalf("v[0]: %d", emulator.v[0x0])
 	}
@@ -138,7 +134,7 @@ func Test_set_vx_vy_substract_vx_vx_greater_than_vy(t *testing.T) {
 	emulator := Make_chip8(500)
 	emulator.v[0x0] = 0b10000000
 	emulator.v[0x2] = 0b00111100
-	emulator.exec_opcode_8_ins([2]byte{0x80,0x27})
+	emulator.exec_opcode_8_ins([2]byte{0x80, 0x27})
 	if emulator.v[0x0] != 0b10111100 || emulator.v[0xf] != 0x0 {
 		t.Fatalf("v[0]: %d", emulator.v[0x0])
 	}
@@ -148,7 +144,7 @@ func Test_set_vx_vy_substract_vx_vx_less_than_vy(t *testing.T) {
 	emulator := Make_chip8(500)
 	emulator.v[0x0] = 0b01111111
 	emulator.v[0x2] = 0b11111111
-	emulator.exec_opcode_8_ins([2]byte{0x80,0x27})
+	emulator.exec_opcode_8_ins([2]byte{0x80, 0x27})
 	if emulator.v[0x0] != 0b10000000 || emulator.v[0xf] != 0x1 {
 		t.Fatalf("v[0]: %d", emulator.v[0x0])
 	}
@@ -157,13 +153,111 @@ func Test_set_vx_vy_substract_vx_vx_less_than_vy(t *testing.T) {
 func Test_set_vx_vx_shift_left(t *testing.T) {
 	emulator := Make_chip8(500)
 	emulator.v[0x0] = 0b10000011
-	emulator.exec_opcode_8_ins([2]byte{0x80,0x2e})
+	emulator.exec_opcode_8_ins([2]byte{0x80, 0x2e})
 	if emulator.v[0x0] != 0b00000110 || emulator.v[0xf] != 0x1 {
 		t.Fatalf("v[0]: %d", emulator.v[0x0])
 	}
 	emulator.v[0x0] = 0b00000111
-	emulator.exec_opcode_8_ins([2]byte{0x80,0x2e})
+	emulator.exec_opcode_8_ins([2]byte{0x80, 0x2e})
 	if emulator.v[0x0] != 0b00001110 || emulator.v[0xf] != 0x0 {
 		t.Fatalf("v[0]: %d", emulator.v[0x0])
+	}
+}
+
+func Test_set_vx_d_timer(t *testing.T) {
+	emulator := Make_chip8(500)
+	emulator.d_timer = 0xf1
+	emulator.exec_opcode_f_ins([2]byte{0xf1, 0x07})
+	if emulator.v[0x1] != 0xf1 {
+		t.Fatalf("v[1]: %d", emulator.v[0x1])
+	}
+}
+
+func Test_set_d_timer_vx(t *testing.T) {
+	emulator := Make_chip8(500)
+	emulator.v[0x1] = 0xf1
+	emulator.exec_opcode_f_ins([2]byte{0xf1, 0x15})
+	if emulator.d_timer != 0xf1 {
+		t.Fatalf("delay timer: %d", emulator.d_timer)
+	}
+}
+
+func Test_set_s_timer_vx(t *testing.T) {
+	emulator := Make_chip8(500)
+	emulator.v[0x1] = 0xf1
+	emulator.exec_opcode_f_ins([2]byte{0xf1, 0x18})
+	if emulator.s_timer != 0xf1 {
+		t.Fatalf("delay timer: %d", emulator.d_timer)
+	}
+}
+
+func Test_set_i_i_add_vx(t *testing.T) {
+	emulator := Make_chip8(500)
+	emulator.i = 100
+	emulator.v[0x2] = 200
+	emulator.exec_opcode_f_ins([2]byte{0xf2, 0x1e})
+	if emulator.i != 300 {
+		t.Fatalf("i: %d", emulator.i)
+	}
+}
+
+func Test_set_i_font_location(t *testing.T) {
+	emulator := Make_chip8(500)
+	emulator.v[0x2] = 0xd
+	emulator.exec_opcode_f_ins([2]byte{0xf2, 0x29})
+	if emulator.i != 65 {
+		t.Fatalf("i: %d", emulator.i)
+	}
+}
+
+func Test_bcd(t *testing.T) {
+	emulator := Make_chip8(500)
+	emulator.v[0x2] = 203
+	emulator.i = 0x300
+	emulator.exec_opcode_f_ins([2]byte{0xf2, 0x33})
+	if emulator.mem[0x300] != 2 || emulator.mem[0x301] != 0 || emulator.mem[0x302] != 3 {
+		t.Fatalf("mem i: %d\nmem i+1: %d\nmem i+2: %d\n", emulator.mem[emulator.i], emulator.mem[emulator.i+1], emulator.mem[emulator.i+2])
+	}
+}
+
+func Test_store_register(t *testing.T) {
+	emulator := Make_chip8(500)
+	emulator.v[0x0] = 0xfa
+	emulator.v[0x1] = 0x1b
+	emulator.v[0x2] = 0xff
+	emulator.v[0x3] = 0x25
+	emulator.i = 0x200
+	emulator.exec_opcode_f_ins([2]byte{0xf3, 0x55})
+	if emulator.mem[0x200] != 0xfa || emulator.mem[0x201] != 0x1b || emulator.mem[0x202] != 0xff || emulator.mem[0x203] != 0x25 {
+		t.Failed()
+	}
+}
+
+func Test_read_register(t *testing.T) {
+	emulator := Make_chip8(500)
+	emulator.mem[0x300] = 0xfa
+	emulator.mem[0x301] = 0x1b
+	emulator.mem[0x302] = 0xff
+	emulator.mem[0x303] = 0x25
+	emulator.i = 300
+	emulator.exec_opcode_f_ins([2]byte{0xf3, 0x65})
+	if emulator.v[0] != 0xfa || emulator.v[1] != 0x1b || emulator.v[2] != 0xff || emulator.v[3] != 0x25 {
+		t.Failed()
+	}
+}
+
+func Test_set_vx_key(t *testing.T) {
+	emulator := Make_chip8(500)
+	emulator.pc = 0x215
+	emulator.exec_opcode_f_ins([2]byte{0xf0, 0x0a})
+	if emulator.pc != 0x213 {
+		t.Fatalf("pc: %x\n", emulator.pc)
+	}
+	emulator.mem[__KB_POS] = 0x1
+	emulator.mem[__KB_POS+1] = 0x5
+	emulator.pc = 0x215
+	emulator.exec_opcode_f_ins([2]byte{0xf0, 0x0a})
+	if emulator.pc != 0x215 || emulator.v[0] != 0x5 {
+		t.Failed()
 	}
 }
